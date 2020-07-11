e9842ba5220ccd17abfc7a9abd78dcde43135aad
shashankv05 <shashank.verma541@gmail.com>
2020-07-11 16:42:04 +0530

    Data Types in R, Conditional Statements, Loops, Operators

---
+++ RBasics.R
@@ -0,0 +1,201 @@
+vtr1 = c(TRUE,FALSE)
+
+class(vtr1)
+vtr2 = c(15,7.45,99999)
+class(vtr2)
+
+vtr3 = c(35L,58L,145L)
+class(vtr3)
+
+#vtr4 = c(211.56L)
+#class(vtr4)
+
+vtr5 = c(TRUE,45L,3.14)
+class(vtr5)
+
+vtr6 = c("Hello",TRUE,45L)
+class(vtr6)
+  
+#Matrix
+mtr = matrix(c(5:29),5,5)
+
+#mtr_2 = matrix(c(5:9),5,1)
+#mtr_3 = matrix(c(5:9),1,5)
+
+
+#Array
+arr_1 =array(c(0:15),dim = c(4,4,2,2))
+arr_2 = array(c(5:9),dim =c(1,5,2,2))
+
+arr_3 = array(c(1:6), dim = c(3,3,2,2))
+arr_4 = array(c(1:6), dim =c(2,2,2))
+arr_5 = array(c(1:6),dim =c(2,1,2,3))
+
+#list
+mylist_ = list(vtr1,vtr2,vtr3)
+
+#Data_frame
+
+vtr1 = c(1:4)
+vtr2 = c("Shashank","Rajesh","Sarita","Gutar")
+vtr3 = c(5,24,19,3)
+data.frame(vtr1,vtr2,vtr3)
+data.frame(airquality)
+data.frame(mtcars)
+
+
+print(2^7)
+
+#Modulus Division
+print(6%%7)
+
+
+print(49/7)
+#Floor Division
+print(22%/%7)
+print(6%/%7)
+
+val_1 = c(TRUE,FALSE,TRUE,FALSE)
+val_2 = c(FALSE,TRUE,TRUE,FALSE)
+print(val_1&val_2)
+print(val_1&&val_2)
+
+print(val_1|val_2)
+print(val_1||val_2)
+
+
+print(!val_1)
+
+
+
+switch(4,
+       "1"= print("Monday"),
+       "2"= print("Tuesday"),
+       "3" = print("Wednesday"),
+       "4" = print("Thursday"),
+       "5" = print("Friday"),
+       "6" = print("Saturday"),
+       "7" = print("Sunday"),
+       print("Invalid Case Expression")
+       )
+
+
+#Difference between repeat and while
+
+var_1 = 2
+
+repeat{
+  if(var_1>2)
+    break
+  print(var_1)
+  var_1 = var_1 + 1
+}
+
+var_1 = 2
+while(var_1>2){
+  print(var_1)
+  var_1 = var_1 + 1
+  }
+
+# for loop
+
+for (variable in 1:25) {
+  
+  print(variable)
+  
+}
+
+#Strings
+
+str1 = "Hey, Shashank!"
+str2 = 'Hey, Beautiful'
+
+str1 <- 'Hey, Shashank!'
+str2 <- 'Hey, Beautiful!'
+
+print(str1)
+print(str2)
+
+str3 = "This is Shashank's"
+print(str3)
+str4 = 'I really love \'Tesla\''
+str5 =  'I love "Tesla" Model S'
+
+print(str4)
+print(str5)
+
+cat('I love "Tesla" Model S')
+
+str_concat = paste(str4,str5)
+print(str_concat)
+
+cat(str_concat)
+nchar(str4)
+nchar(str5)
+nchar(str_concat)
+
+str_upper = toupper(str4)
+str_lower = tolower(str4)
+
+print(str_upper)
+print(str_lower)
+
+
+
+str_substring = substr(str_concat,9,21)
+print(str_substring)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
